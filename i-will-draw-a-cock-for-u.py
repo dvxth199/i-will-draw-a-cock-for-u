3d03edb261cffe6ea762e52ad5d8f5305202ec6e
D <77586623+dvxth19@users.noreply.github.com>
2021-03-29 02:29:19 +0300

    Add files via upload

---
+++ i-will-draw-a-cock-for-u.py
@@ -0,0 +1,53 @@
+import turtle
+turtle.speed(199)
+
+turtle.circle(50)
+turtle.penup()
+turtle.goto(190,0)
+turtle.pendown()
+turtle.circle(50)
+turtle.penup()
+turtle.goto(60,50)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(200)
+
+turtle.right(30)
+turtle.forward(20)
+turtle.right(30)
+turtle.forward(20)
+turtle.right(30)
+turtle.forward(20)
+turtle.right(30)
+turtle.forward(20)
+turtle.right(30)
+turtle.forward(20)
+turtle.right(30)
+turtle.forward(20)
+
+turtle.forward(180)
+turtle.penup()
+turtle.goto(60,220)
+turtle.left(90)
+turtle.pendown()
+turtle.forward(75)
+turtle.penup()
+turtle.goto(98,220)
+turtle.left(90)
+turtle.pendown()
+turtle.forward(60)
+turtle.penup()
+turtle.goto(0,0)
+
+
+
+print('😳')
+print('''C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   
+C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   
+C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   
+C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   
+C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   
+C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   
+C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   
+C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   C O C K   ''')
+input()
